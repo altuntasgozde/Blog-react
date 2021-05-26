186d65913fd6052b3ae8099631e18ec010cc82d9
Gözde Altuntaş <altuntasgozde@gmail.com>
2021-05-26 11:55:51 +0300

    pagination

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import {
-  Card,
-  CardText,
-  CardTitle,
-  Col,
-  Container,
-  Row,
-} from "reactstrap";
+import { Card, CardText, CardTitle, Col, Container, Row } from "reactstrap";
+import Pagination from "./Pagination";
 
-function HomePage({blogPosts}) {
+function HomePage({ blogPosts }) {
+  const [postsPerPage] = useState(5);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = blogPosts.slice(indexOfFirstPost, indexOfLastPost);
 
   return (
     <div>
@@ -17,24 +21,33 @@ function HomePage({blogPosts}) {
         <Row>
           <Col md={{ size: 8, offset: 2 }}>
             <div>
-              {blogPosts.map((post, index) => (
+              {currentPosts.map((post, index) => (
                 <Card className="mt-5" body key={index}>
                   <CardTitle tag="h5">{post.blogTitle}</CardTitle>
                   <CardText>
                     {post.blogPost.slice(0, 360).concat("...")}
-                    <Link style={{textDecoration: 'none'}} to={"/post/" + post.id} className="readMore">
+                    <Link
+                      style={{ textDecoration: "none" }}
+                      to={"/post/" + post.id}
+                      className="readMore"
+                    >
                       Read more
                     </Link>
                   </CardText>
                   <CardText>
-                    <small className="text-muted">
-                    {post.date}
-                    </small>
+                    <small className="text-muted">{post.date}</small>
                   </CardText>
                 </Card>
-              )).reverse()}
+              ))}
             </div>
           </Col>
+        </Row>
+        <Row className="pagination">
+            <Pagination
+              postsPerPage={postsPerPage}
+              totalPosts={blogPosts.length}
+              paginate={paginate}
+            />
         </Row>
       </Container>
     </div>
